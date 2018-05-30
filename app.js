00d94c238cdd1cad8f78029177b730bb397b452a
natalia <natalia.lucerot@gmail.com>
2018-05-30 15:38:58 -0500

    Ejercicio edades y etapas completo

---
+++ app.js
@@ -0,0 +1,22 @@
+const age = prompt("¿Que edad tienes?");
+if (age < 3){
+    alert("estas dentro del grupo de los toddler");
+}else if (age < 6){
+    alert("estas dentro del grupo de los preschool");
+} else if (age < 13){
+    alert("estas dentro del grupo de los gradeschooler");
+}else if (age < 19){
+    alert("estas dentro del grupo de los teenager");
+}else if(age < 22){
+    alert("estas dentro del grupo de los young adult");
+}else if(age > 22){
+    alert("estas dentro del grupo de los adult");
+}
+else {
+    alert("¡Error!");
+} 
+
+
+
+
+
